cf341baff7916327916c51c5d5bbc343b07a9f16
Arsen Temurian <arsentim2611@gmail.com>
2021-06-12 02:11:16 +0300

    ДЗ№7 Уже похоже на правду

--- js/modal.js
+++ js/modal.js
@@ -9,15 +9,11 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('backdrop--is-hidden');
   }
 })();
 
 // console.log(document.querySelector(".header-page").getBoundingClientRect().height);
 
-
-const { height: headerPageHeight } = document
-  .querySelector('.header-page')
-  .getBoundingClientRect();
+const { height: headerPageHeight } = document.querySelector('.page-header').getBoundingClientRect();
 document.body.style.paddingTop = `${headerPageHeight}px`;
-  
